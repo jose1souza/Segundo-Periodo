65d37d6668bb963a2cde6aeed1350f2b625e20cb
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2024-09-25 20:24:43 -0300

    Add files via upload

---
+++ funcaopotencia.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static double CalcularPotencia(int num,int x)
+    {
+        return Math.Pow(num, x); ;
+        // fim da função potencia
+    }
+        static void Main()
+        {
+            int n1, ex;
+            // declarando váriaveis
+
+            Console.WriteLine("Entre com um valor: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o valor do expoente: ");
+            ex = int.Parse(Console.ReadLine());
+            // lendo valores
+
+            double res = CalcularPotencia(n1, ex);
+            // chamando a função potencia
+
+            Console.WriteLine($"{res} é o resultado de {n1} elevado a {ex}");
+            // mostrando o resultado
+
+            Console.ReadKey();
+
+
+        } // fim do main
+    } // fim do programa
