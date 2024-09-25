031d47d2180fc96e3ae288d2b3371cf82825caba
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2024-09-25 19:12:20 -0300

    Add files via upload

---
+++ funcao.primo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static bool EhPrimo(int num)
+    {
+        for (int i = 2; i < num; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+                // número não é primo
+            }
+        }
+        return true;
+    }// o número é primo
+     // fim da função primo
+    static void Main()
+    {
+        int n1;
+        // declarando váriaveis
+        Console.WriteLine("Entre com um valor: ");
+        n1 = int.Parse(Console.ReadLine());
+        // lendo valor para verificar se é primo
+        bool primo = EhPrimo(n1);
+        // chamando a função
+
+        if (primo == true)
+        {
+            Console.WriteLine($"{n1} é um número primo");
+        }
+        else
+        {
+            Console.WriteLine($"{n1} não é um número primo");
+        }
+        // mostrando o resultado
+
+        Console.ReadKey();
+
+
+    } // fim do main
+} // fim do programa
