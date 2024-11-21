1a106482d75041b89ff7841a488eb2cd59b05a11
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2024-11-21 13:19:34 -0300

    Add files via upload

---
+++ Revisão_da_Prova/revisao-main/revisao-main/revisao_prova/Exercicio2.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Biblioteca_Matriz;
+
+namespace revisao_prova
+{
+    public class Exercicio2
+    {
+        public static int somatoda(int[,] mat)
+        {
+            int linhas = mat.GetLength(0);
+            int cols = mat.GetLength(1);
+            int soma = 0;
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    soma = soma + mat[i, j];
+                }// fim for j
+            }// fim for i
+            return soma;
+        }
+        public static int somacoluna(int[,] mat, int col)
+        {
+            int linhas = mat.GetLength(0);
+            int soma = 0;
+            for (int i = 0; i < linhas; i++)
+            {
+                soma = soma + mat[i, col];
+                Console.WriteLine();
+            }// fim for i
+            return soma;
+        }
+        public static int somalinha(int[,] mat, int lin)
+        {
+            int cols = mat.GetLength (1);
+            int soma = 0;
+            for (int j = 0; j < cols; j++)
+            {
+                soma = soma + mat[lin, j];
+            }// fim for i
+            return soma;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Linhas e colunas");
+
+            int linhas = int.Parse(Console.ReadLine());
+            int colunas = int.Parse(Console.ReadLine());
+
+            int[,] minhaMatriz = new int[linhas, colunas];
+
+            Matriz.gera(minhaMatriz);
+            Console.WriteLine("Matriz Gerada: ");
+            Matriz.mostra(minhaMatriz);
+
+            Console.WriteLine("Coluna para somar: ");
+            int coluna_soma = int.Parse(Console.ReadLine());
+            int resultado_coluna = somacoluna(minhaMatriz, coluna_soma);
+            Console.WriteLine($"Soma da coluna {coluna_soma} é: {resultado_coluna}");
+
+            Console.WriteLine("Linha para somar: ");
+
+            int linha_soma = int.Parse(Console.ReadLine());
+
+            int resultado_linha = somalinha(minhaMatriz, linha_soma);
+
+            Console.WriteLine($"Soma da linha {coluna_soma} é: {resultado_linha}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Revisão_da_Prova/revisao-main/revisao-main/revisao_prova/Exercicio3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace revisao_prova
+{
+     class Exercicio3
+    {
+        static void Main()
+        {
+            Console.WriteLine("Exercicio 3");
+            Console.WriteLine("Entre com uma palavra");
+            string palavra = Console.ReadLine();
+
+            Console.WriteLine($"Minha string:{palavra}");
+            Console.WriteLine("Posicao para acessar:");
+            int pos = int.Parse(Console.ReadLine());
+            Console.WriteLine("Caracter");
+            Console.WriteLine($"{palavra[pos]}");
+            Console.WriteLine("ASCII");
+            Console.WriteLine($"{(int)palavra[pos]}");
+            char[] vetorChar = palavra.ToCharArray();
+            Console.WriteLine("Posicao em minusculo");
+            vetorChar[pos] = (char)(vetorChar[pos] + 32);
+            Console.WriteLine($"{vetorChar[pos]}");
+            Console.WriteLine("Palavra modificada:");
+            for (int i = 0; i < vetorChar.Length; i++)
+            {
+                Console.Write(vetorChar[i]);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
